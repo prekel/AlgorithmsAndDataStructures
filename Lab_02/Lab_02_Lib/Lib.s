0d319b8ed35f4fd8bdd49c80cf7b968d50e7c52b
Vladislav Prekel <misterptits@yandex.ru>
2020-05-12 18:46:05 +0700

    average

--- Lab_02/Lab_02_Lib/Lib.s
+++ Lab_02/Lab_02_Lib/Lib.s
@@ -52,36 +52,48 @@ Loop1_End:
 # double AverageInArray(const int* array, int count);
         .globl  AverageInArray
 AverageInArray:
-pushq   %rbp
+        # пролог
+        pushq   %rbp
         movq    %rsp, %rbp
-        movq    %rdi, -24(%rbp)
-        movl    %esi, -28(%rbp)
-        pxor    %xmm0, %xmm0
-        movsd   %xmm0, -8(%rbp)
-        movl    $0, -12(%rbp)
-.L8:
-        movl    -12(%rbp), %eax
-        cmpl    -28(%rbp), %eax
-        jge     .L7
-        movl    -12(%rbp), %eax
-        cltq
-        leaq    0(,%rax,4), %rdx
-        movq    -24(%rbp), %rax
-        addq    %rdx, %rax
-        movl    (%rax), %eax
-        pxor    %xmm0, %xmm0
-        cvtsi2sdl       %eax, %xmm0
-        movsd   -8(%rbp), %xmm1
-        addsd   %xmm1, %xmm0
-        movsd   %xmm0, -8(%rbp)
-        addl    $1, -12(%rbp)
-        jmp     .L8
-.L7:
-        pxor    %xmm1, %xmm1
-        cvtsi2sdl       -28(%rbp), %xmm1
-        movsd   -8(%rbp), %xmm0
-        divsd   %xmm1, %xmm0
-        movq    %xmm0, %rax
-        movq    %rax, %xmm0
+        # запись в стек адреса первого элемента массива -24(%rbp)
+        # он будет смещатся чтобы получить нужный элемент
+        movq    %rdi, -24(%rbp)                 # -24(%rbp) <- array
+        # запись в стек кол-ва элементов -28(%rbp)
+        movl    %esi, -28(%rbp)                 # -28(%rbp) <- count
+        pxor    %xmm0, %xmm0                    # %xmm0 <- 0
+        movsd   %xmm0, -12(%rbp)                # -12(%rbp) <- %xmm0
+        # номер текущего элемента -4(%rbp)
+        movl    $0, -4(%rbp)                    # -4(%rbp) <- 0
+Loop2_Start:
+        # сравнение номера текущего элемента с кол-вом элементов
+        # если номер текущего равен или больше, то конец цикла
+        movl    -4(%rbp), %eax                  # %eax <- -4(%rbp)
+        cmpl    -28(%rbp), %eax                 # compare -28(%rbp) <= %eax
+        jge     Loop2_End                       # if greater or equal go to Loop1_End
+        # запись текущего элемента в %eax
+        movq    -24(%rbp), %rax                 # %rax <- -24(%rbp)
+        movl    (%rax), %eax                    # %eax <- *(%rax)
+        # записать текущий элемент в регистр с плавающей точкой
+        cvtsi2sdl       %eax, %xmm0             # %xmm0 <- %eax
+        # увеличить сумму на текущий элемент
+        movsd   -12(%rbp), %xmm1                # %xmm1 <- -12(%rbp)
+        addsd   %xmm1, %xmm0                    # %xmm0 += %xmm1
+        movsd   %xmm0, -12(%rbp)                # -12(%rbp) <- %xmm0
+        # номер текущего увеличивается на 1
+        addl    $1, -4(%rbp)                    # -4(%rbp) += 1
+        # адрес текущего увеличивается на 4 (байта)
+        movq    -24(%rbp), %rax                 # %rax <- -24(%rbp)
+        addq    $4, %rax                        # %rax += 4
+        movq    %rax, -24(%rbp)                 # -24(%rbp) <- %rax
+        # цикл идёт с начала
+        jmp     Loop2_Start
+Loop2_End:
+        # записываем кол-во элементов в регистр с плавающей точкой
+        cvtsi2sdl       -28(%rbp), %xmm1        # %xmm1 <- -28(%rbp)
+        # делим сумму на кол-во элементов
+        # результат будет в регистре %xmm0 который возвращается
+        movsd   -12(%rbp), %xmm0                # %xmm0 <- -12(%rbp)
+        divsd   %xmm1, %xmm0                    # %xmm0 /= %xmm1
+        # эпилог
         popq    %rbp
         ret
