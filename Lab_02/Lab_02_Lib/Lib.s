b0f836dc06a151479f6ab59f7d3b8ce657778579
Vladislav Prekel <misterptits@yandex.ru>
2020-05-12 17:22:10 +0700

    asm1

--- Lab_02/Lab_02_Lib/Lib.s
+++ Lab_02/Lab_02_Lib/Lib.s
@@ -1,96 +1,83 @@
-	.text
-	.file	"Lib.c"
-	.globl	MaxInArray              # -- Begin function MaxInArray
-	.p2align	4, 0x90
-	.type	MaxInArray,@function
-MaxInArray:                             # @MaxInArray
-	.cfi_startproc
-# %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	movq	%rdi, -8(%rbp)
-	movl	%esi, -12(%rbp)
-	movq	-8(%rbp), %rdi
-	movl	(%rdi), %esi
-	movl	%esi, -16(%rbp)
-	movl	$0, -20(%rbp)
-.LBB0_1:                                # =>This Inner Loop Header: Depth=1
-	movl	-20(%rbp), %eax
-	cmpl	-12(%rbp), %eax
-	jge	.LBB0_6
-# %bb.2:                                #   in Loop: Header=BB0_1 Depth=1
-	movl	-16(%rbp), %eax
-	movq	-8(%rbp), %rcx
-	movslq	-20(%rbp), %rdx
-	cmpl	(%rcx,%rdx,4), %eax
-	jge	.LBB0_4
-# %bb.3:                                #   in Loop: Header=BB0_1 Depth=1
-	movq	-8(%rbp), %rax
-	movslq	-20(%rbp), %rcx
-	movl	(%rax,%rcx,4), %edx
-	movl	%edx, -16(%rbp)
-.LBB0_4:                                #   in Loop: Header=BB0_1 Depth=1
-	jmp	.LBB0_5
-.LBB0_5:                                #   in Loop: Header=BB0_1 Depth=1
-	movl	-20(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -20(%rbp)
-	jmp	.LBB0_1
-.LBB0_6:
-	movl	-16(%rbp), %eax
-	popq	%rbp
-	retq
-.Lfunc_end0:
-	.size	MaxInArray, .Lfunc_end0-MaxInArray
-	.cfi_endproc
-                                        # -- End function
+        .text
 
-	.globl	AverageInArray          # -- Begin function AverageInArray
-	.p2align	4, 0x90
-	.type	AverageInArray,@function
-AverageInArray:                         # @AverageInArray
-	.cfi_startproc
-# %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	xorps	%xmm0, %xmm0
-	movq	%rdi, -8(%rbp)
-	movl	%esi, -12(%rbp)
-	movsd	%xmm0, -24(%rbp)
-	movl	$0, -28(%rbp)
-.LBB2_1:                                # =>This Inner Loop Header: Depth=1
-	movl	-28(%rbp), %eax
-	cmpl	-12(%rbp), %eax
-	jge	.LBB2_4
-# %bb.2:                                #   in Loop: Header=BB2_1 Depth=1
-	movq	-8(%rbp), %rax
-	movslq	-28(%rbp), %rcx
-	movl	(%rax,%rcx,4), %edx
-	cvtsi2sdl	%edx, %xmm0
-	addsd	-24(%rbp), %xmm0
-	movsd	%xmm0, -24(%rbp)
-# %bb.3:                                #   in Loop: Header=BB2_1 Depth=1
-	movl	-28(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -28(%rbp)
-	jmp	.LBB2_1
-.LBB2_4:
-	movsd	-24(%rbp), %xmm0        # xmm0 = mem[0],zero
-	movl	-12(%rbp), %eax
-	cvtsi2sdl	%eax, %xmm1
-	divsd	%xmm1, %xmm0
-	popq	%rbp
-	retq
-.Lfunc_end2:
-	.size	AverageInArray, .Lfunc_end2-AverageInArray
-	.cfi_endproc
-                                        # -- End function
+# int MaxInArray(const int* array, int count);
+        .globl  MaxInArray
+MaxInArray:
+        # пролог
+        pushq   %rbp
+        movq    %rsp, %rbp
+        # запись в стек адреса первого элемента массива -24(%rbp)
+        movq    %rdi, -24(%rbp)                 # -24(%rbp) <- array
+        # запись в стек кол-ва элементов -28(%rbp)
+        movl    %esi, -28(%rbp)                 # -28(%rbp) <- count
+        # максимум (в начале первый элемент массива) -4(%rbp)
+        movq    -24(%rbp), %rax                 # %rax <- array
+        movl    (%rax), %eax                    # %eax <- (%rax)[0]
+        movl    %eax, -4(%rbp)                  # -4(%rbp) <- %eax
+        # итератор -8(%rbp)
+        movl    $0, -8(%rbp)                    # -8(%rbp) <- 0
+Loop1_Start:
+        # сравнение итератора с кол-вом элементов
+        movl    -8(%rbp), %eax                  # %eax <- -8(%rbp)
+        cmpl    -28(%rbp), %eax                 # compare -28(%rbp), %eax
+        jge     Loop1_End                       # if greater or equal go to Loop1_End
+        # вычысление адреса
+        movl    -8(%rbp), %eax                  # %eax <- -8(%rbp)
+        cltq                                    # %rax <- %eax
+        leaq    0(,%rax,4), %rdx                # %rdx <- %rax * 4
+        movq    -24(%rbp), %rax                 # %rax <- -24(%rbp)
+        addq    %rdx, %rax                      # %rax += %rdx
+        movl    (%rax), %eax                    # %eax <- *(%rax)
+        cmpl    %eax, -4(%rbp)                  # compare %eax, -4(%rbp)
+        jge     Loop1_Continue
+        movl    -8(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movl    (%rax), %eax
+        movl    %eax, -4(%rbp)
+Loop1_Continue:
+        addl    $1, -8(%rbp)
+        jmp     Loop1_Start
+Loop1_End:
+        movl    -4(%rbp), %eax
+        popq    %rbp
+        ret
 
-	.ident	"clang version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)"
-	.section	".note.GNU-stack","",@progbits
+# double AverageInArray(const int* array, int count);
+        .globl  AverageInArray
+AverageInArray:
+pushq   %rbp
+        movq    %rsp, %rbp
+        movq    %rdi, -24(%rbp)
+        movl    %esi, -28(%rbp)
+        pxor    %xmm0, %xmm0
+        movsd   %xmm0, -8(%rbp)
+        movl    $0, -12(%rbp)
+.L8:
+        movl    -12(%rbp), %eax
+        cmpl    -28(%rbp), %eax
+        jge     .L7
+        movl    -12(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movl    (%rax), %eax
+        pxor    %xmm0, %xmm0
+        cvtsi2sdl       %eax, %xmm0
+        movsd   -8(%rbp), %xmm1
+        addsd   %xmm1, %xmm0
+        movsd   %xmm0, -8(%rbp)
+        addl    $1, -12(%rbp)
+        jmp     .L8
+.L7:
+        pxor    %xmm1, %xmm1
+        cvtsi2sdl       -28(%rbp), %xmm1
+        movsd   -8(%rbp), %xmm0
+        divsd   %xmm1, %xmm0
+        movq    %xmm0, %rax
+        movq    %rax, %xmm0
+        popq    %rbp
+        ret
